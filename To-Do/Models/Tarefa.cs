6d2d8f7502ad960517bb34516356420ce75195d7
patrick Rafael <PatrickRafael05@gmail.com>
2023-05-11 10:41:50 -0300

    Configurando e Adicionando banco de dados

--- To-Do/Models/Tarefa.cs
+++ To-Do/Models/Tarefa.cs
@@ -1,6 +1,19 @@
-﻿namespace To_Do.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace To_Do.Models
 {
     public class Tarefa
     {
+        public int TarefaId { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "Use Menos Caracteres") ]
+        public String Nome { get; set; }
+
+        public String Data { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório ")]
+        [DataType(DataType.Time)] 
+        public String Horario { get; set; }
     }
 }
